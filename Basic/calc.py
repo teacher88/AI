f34db86a1aeb2028ba46301f29d149ec4017d121
DESKTOP-LBEO3IF\JMH <lucia88@hanmail.net>
2025-02-17 12:41:40 +0900

    VER18

---
+++ Basic/calc.py
@@ -0,0 +1,31 @@
+# 함수
+def plus(a, b):
+    return a+b
+
+def minus(a, b):
+    return a-b
+
+def multiply(a, b):
+    return a*b
+
+def divide(a, b):
+    return a/b
+    
+# 클래스
+class Su:
+    def __init__(self, x, y):    # su=Su(10, 20)
+        self.x=x
+        self.y=y
+
+    def disp(self):
+        print("%d, %d" %(self.x, self.y))
+        
+# 변수
+fruit="바나나"
+
+
+
+
+
+
+
